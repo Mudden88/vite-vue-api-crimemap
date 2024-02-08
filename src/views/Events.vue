19732ad2a2db2eef190c52f3ac7817e29f000535
Mudden88 <mudden@live.se>
2024-02-08 22:16:38 +0100

    modified:   src/views/Events.vue

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -17,24 +17,18 @@ import UserName from '../components/UserName.vue'
     <p class="pfirst" v-if="yourName"> Hej! {{ yourName }}, Du har valt: {{ selectedOpt }} </p>
 
     <div class="cardWrapper">
-
-
       <ul v-for=" event  in  events " :key="event.id">
         <div class="card" @click="selectEvent(event)">
           <p class="id">Id: {{ event.id }}</p>
           <hr>
-          <p class="sText">
-            {{ event.pubdate_iso8601 }}
-          </p>
-          <p>{{ event.title_location }},
-            {{ event.title_type }}
-          </p>
+          <p class="sText">{{ event.pubdate_iso8601 }}</p>
+          <p>{{ event.title_location }},{{ event.title_type }}</p>
           <p>{{ event.description }}</p>
-
           <p>{{ event.content_teaser }}</p>
           <a :href="`${event.permalink}`" target="_blank">Läs mer...</a>
         </div>
       </ul>
+
       <!-- Visar att datan håller på att laddas in så länge if villkoret är true, villkoret ändras till false i .finally i axios fetchen:  -->
       <div class="loading" v-if="loading == true">
         <img src="/assets/Spinner-1s-200px.gif" alt="Image by Pngtree" width="80" height="80">
@@ -42,7 +36,6 @@ import UserName from '../components/UserName.vue'
 
     </div>
     <div class="imgMap" v-if="selectedImage">
-
       <img class="Image" :src="selectedImage.image" alt="karta">
     </div>
   </div>
@@ -73,13 +66,13 @@ export default {
         { value: 'Åmål', label: 'Åmål' },
         { value: 'Mölndal', label: 'Mölndal' },
         { value: 'Gotland', label: 'Gotland' },
-        { value: 'Öland', label: 'Öland' }
+        { value: 'Öland', label: 'Öland' },
+        { value: 'Reftele', label: 'Gislaved' }
       ]
     }
   },
 
   mounted() {
-
     //Sorterar options i bokstavsordning
     this.options.sort((a, b) => a.label.localeCompare(b.label, 'sv'))
     //setTimeout för att fördröja svaret något
@@ -90,20 +83,14 @@ export default {
         })
         .finally(() => {
           this.loading = false
-
         })
     }, "2000")
-
   },
-
   watch: {
     selectedOpt(newOption) {
       this.fetchEvents(newOption)
     },
-
-
   },
-
   methods: {
     fetchEvents(city) {
       this.loading = true
@@ -124,12 +111,8 @@ export default {
     handleNewUser(payload) {
       this.yourName = payload
     }
-
-
-
   },
   emits: ['custom-event'],
-
 }
 
 
