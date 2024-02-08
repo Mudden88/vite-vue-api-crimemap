d158df82dcf27b56886845d3dac13e297b09058f
Mudden88 <mudden@live.se>
2024-02-08 21:51:48 +0100

    new file:   src/components/UserName.vue

---
+++ src/components/UserName.vue
@@ -0,0 +1,40 @@
+<template>
+  <label class="pfirst">Skriv in ditt namn:
+    <input type="text" v-model="payload">
+    <button @click="setUser">Skicka</button>
+  </label>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      payload: this.yourName
+    }
+  },
+  methods: {
+    setUser() {
+      this.$emit('new-user', this.payload)
+    }
+  },
+  props: {
+    yourName: {
+      type: String,
+      validator: function (yourName) {
+        if (typeof yourName === 'string' && yourName !== null) {
+          return true
+        }
+        return false
+
+      }
+    }
+  },
+  emits: ['new-user']
+}
+</script>
+
+<style scoped>
+input {
+  width: 10em;
+}
+</style>
